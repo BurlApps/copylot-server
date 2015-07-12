4f4ac5e8fc334f3f919dc8f6ad78c4765fbf4377
Brian Vallelunga <vallelungabrian@gmail.com>
2015-07-11 20:09:40 -0700

    changed it to alter

--- config/env/production.js
+++ config/env/production.js
@@ -19,7 +19,7 @@ module.exports = {
 
   models: {
     connection: 'production',
-    migrate: 'drop'
+    migrate: 'alter'
   },
 
   /***************************************************************************
